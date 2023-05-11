ed86e579659c59a1f4b11463d23a005fb574d3a4
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-05-11 15:16:19 +0100

    Updated mockup frontend to be compatible with backend

--- backend/src/main/webapp/register/app.js
+++ backend/src/main/webapp/register/app.js
@@ -12,32 +12,12 @@ function register() {
             let username = document.getElementById("username").value;
             let name = document.getElementById("name").value;
             let email = document.getElementById("email").value;
-            let landline = document.getElementById("landline").value;
-            let mobile = document.getElementById("mobile").value;
-            let occupation = document.getElementById("occupation").value;
-            let workplace = document.getElementById("workplace").value;
-            let address = document.getElementById("address").value;
-            let complementary = document.getElementById("complementary").value;
-            let city = document.getElementById("city").value;
-            let postcode = document.getElementById("postcode").value;
-            let nif = document.getElementById("nif").value;
-            let privacy = document.getElementById("privacy").value;
             let userData = {
                 "username": username,
                 "name": name,
                 "email": email,
                 "password": password,
                 "confirmation": confirmation,
-                "privacy": privacy,
-                "landline": landline,
-                "mobile": mobile,
-                "occupation": occupation,
-                "workplace": workplace,
-                "address": address,
-                "complementary": complementary,
-                "city": city,
-                "postcode": postcode,
-                "nif": nif
             };
             let xhr = new XMLHttpRequest();
 
@@ -61,37 +41,6 @@ function register() {
     });
 }
 
-function validateToken() {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "/rest/token/validate", true);
-    xhr.send();
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            window.location.href = "/profile";
-        }
-    };
-}
-
-function updateProfilePic() {
-    let selectedFile = document.getElementById("profilePic").files[0];
-    document.getElementsByClassName("profile_picture")[0].src = URL.createObjectURL(selectedFile);
-}
-
-function uploadPFP() {
-    let fileInput = document.getElementById("profilePic");
-    let file = fileInput.files[0];
-
-    if(file !== null) {
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", "/gcs/universe-202223.appspot.com/" + document.getElementById("username").value, false);
-        xhr.setRequestHeader("Content-Type", file.type);
-        xhr.send(file);
-    }
-}
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    validateToken();
     register();
-    uploadPFP();
 });
