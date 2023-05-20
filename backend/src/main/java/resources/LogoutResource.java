0aec1f58af9a63044a8382eca4e0fd0504732045
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-05-20 23:57:00 +0100

    Implemented logout, token is removed from cookies and blacklisted in datastore

--- backend/src/main/java/resources/LogoutResource.java
+++ backend/src/main/java/resources/LogoutResource.java
@@ -1,8 +1,19 @@
 package resources;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.cloud.Timestamp;
+import com.google.cloud.datastore.*;
+import org.apache.commons.codec.digest.DigestUtils;
+import util.ValToken;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 
@@ -10,14 +21,37 @@ import java.util.logging.Logger;
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class LogoutResource {
     private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+    private static final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
     public LogoutResource() { }
 
     @POST
     @Path("/")
-    public Response logout() {
-    // TODO
-        return Response.status(Response.Status.BAD_REQUEST).entity("Logout failed").build();
+    public Response logout(@Context HttpServletRequest request, @Context HttpServletResponse response) {
+        LOG.fine("Attempt to logout user.");
+
+        Transaction txn = datastore.newTransaction();
+
+        try {
+            final ValToken validator = new ValToken();
+            DecodedJWT token = validator.invalidateToken(request, response);
+
+            Key tokenKey = datastore.newKeyFactory().setKind("Token_Blacklist").newKey(token.getId());;
+
+            Entity blToken = Entity.newBuilder(tokenKey)
+                    .set("expiration", Timestamp.of(token.getExpiresAt()))
+                    .build();
+            txn.add(blToken);
+
+
+            LOG.info("User logged out");
+            txn.commit();
+            return Response.ok().build();
+        } finally {
+            if (txn.isActive()) {
+                txn.rollback();
+            }
+        }
     }
 
 
