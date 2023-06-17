39f05b5f4d4e7e76a00529538ffc6ed79667d317
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-06-17 21:57:51 +0100

    Firebase V1

--- backend/src/main/java/resources/RegisterResource.java
+++ backend/src/main/java/resources/RegisterResource.java
@@ -1,162 +1,64 @@
 package resources;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-
 import com.google.cloud.Timestamp;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.*;
-import org.apache.commons.codec.digest.DigestUtils;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import util.UserData;
-import util.ValToken;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Path("/register")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class RegisterResource {
-    private static final Logger LOG = Logger.getLogger(RegisterResource.class.getName());
-    private static final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    private static final Logger LOG = Logger.getLogger(LoginResource.class.getName());
+    private static final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     public RegisterResource() { }
 
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response register(@Context HttpServletRequest request, UserData data) {
-        LOG.fine("Attempt to register user: " + data.email);
+    public Response register(UserData data) {
+        LOG.fine("Attempt to register user: " + data.username);
 
-        if( !data.validateRegister() ) {
+        if (!data.validateRegister()) {
             LOG.warning("Missing or wrong parameter");
             return Response.status(Response.Status.BAD_REQUEST).entity("Missing or wrong parameter").build();
         }
 
-        Transaction txn = datastore.newTransaction();
         try {
-            Key userKey = datastore.newKeyFactory().setKind("User").newKey(data.username);
-            Entity user = txn.get(userKey);
+            UserRecord user = firebaseAuth.createUser( new CreateRequest()
+                    .setUid(data.username)
+                    .setEmail(data.email)
+                    .setEmailVerified(false)
+                    .setPassword(BCrypt.withDefaults().hashToString(12, data.password.toCharArray()))
+                    .setDisplayName(data.name)
+                    .setPhotoUrl("TODO")
+                    .setDisabled(true)
+            );
 
-            if( user != null ) {
-                txn.rollback();
-                LOG.warning("User already exists");
-                return Response.status(Response.Status.BAD_REQUEST).entity("User already exists").build();
-            } else {
+            Map<String, Object> customClaims = new HashMap<>();
+            customClaims.put("role", data.getRole());
+            customClaims.put("time_creation", Timestamp.now());
+            customClaims.put("time_lastupdate", Timestamp.now());
 
-                user = Entity.newBuilder(userKey)
-                        .set("email", data.email)
-                        .set("name", data.name)
-                        .set("password", DigestUtils.sha512Hex(data.password))
-                        .set("role", data.getRole())
-                        .set("status", "ACTIVE")
-                        .set("job_list", "")
-                        .set("time_creation", Timestamp.now())
-                        .set("time_lastupdate", Timestamp.now())
-                        .build();
-                txn.add(user);
+            firebaseAuth.setCustomUserClaims(user.getUid(), customClaims);
 
-                LOG.info("User registered " + data.username);
-                txn.commit();
-                return Response.ok(user).build();
-            }
-        } finally {
-            if (txn.isActive()) {
-                txn.rollback();
-            }
+            LOG.info("User registered: " + user.getUid());
+            return Response.ok(user).build();
+        } catch (FirebaseAuthException e) {
+            LOG.warning("User registration failed: " + e.getMessage());
+            return Response.status(Response.Status.BAD_REQUEST).entity("User registration failed: " + e.getMessage()).build();
         }
     }
-
-    /*@POST
-    @Path("/new/{kind}/{key}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response getReceivedInbox(@Context HttpServletRequest request, @PathParam("kind") String kind, @PathParam("key") String keyName, Map<String, String> attributes) {
-        LOG.fine("Attempt to create new entity");
-
-
-
-        Transaction txn = datastore.newTransaction();
-        Key key = datastore.newKeyFactory().setKind("Role").newKey(keyName);
-
-        try {
-            Entity role = txn.get(key);
-            if(role == null){
-                txn.rollback();
-                return Response.status(Response.Status.NOT_FOUND).build();
-            }
-            Entity.Builder builder = Entity.newBuilder(key);
-            for(Map.Entry<String, String> attribute : attributes.entrySet()) {
-                builder.set(attribute.getKey(), attribute.getValue());
-            }
-            Entity entity = builder.build();
-            txn.put(entity);
-
-            LOG.info("Role Created");
-            txn.commit();
-            return Response.ok().build();
-        } finally {
-            if (txn.isActive()) {
-                txn.rollback();
-            }
-        }
-    }*/
-
-    /*@POST
-    @Path("/v2")
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response registerV2(UserData data) {
-        String[] emails = data.getEmails();
-        for (String email : emails) {
-            String username = email.split("@")[0];
-            LOG.fine("Attempt to register user: " + username);
-
-            if (!data.validateRegister()) {
-                LOG.warning("Missing or wrong parameter");
-                return Response.status(Response.Status.BAD_REQUEST).entity("Missing or wrong parameter").build();
-            }
-
-            Transaction txn = datastore.newTransaction();
-            try {
-                Key userKey = datastore.newKeyFactory().setKind("User").newKey(username);
-                Entity user = txn.get(userKey);
-
-                if (user != null) {
-                    txn.rollback();
-                    LOG.warning("User already exists");
-                    //return Response.status(Response.Status.BAD_REQUEST).entity("User already exists").build();
-                } else {
-                    Entity.Builder builder = Entity.newBuilder(userKey);
-
-                    String password = UUID.randomUUID().toString();
-
-                    builder.set("name", data.name)
-                            .set("password", DigestUtils.sha512Hex(password))
-                            .set("email", data.email)
-                            .set("role", data.role);
-
-                    for (String[] attribute : data.attributes) {
-                        builder.set(attribute[0], attribute[1]);
-                    }
-
-                    user = builder.build();
-                    txn.add(user);
-
-                    LOG.info("User registered " + data.username);
-                    txn.commit();
-                }
-            } finally {
-                if (txn.isActive()) {
-                    txn.rollback();
-                }
-            }
-        }
-        return Response.ok().build();
-    }*/
 
 
 }
