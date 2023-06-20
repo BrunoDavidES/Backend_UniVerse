7b01d9ace4d9e632f2d84923f385b6492601d42b
Manuel <m.jardim@campus.fct.unl.pt>
2023-06-20 16:36:43 +0100

    script ongoing

--- backend/src/main/webapp/js/functions.js
+++ backend/src/main/webapp/js/functions.js
@@ -48,26 +48,293 @@
       }
 	}
 
-  function logout(){
-	var xmlhttp = new XMLHttpRequest();
+function logout(){
+    var xmlhttp = new XMLHttpRequest();
 
 
-	var j = {}
+    var j = {}
 
-	var json = JSON.stringify(j);
+    var json = JSON.stringify(j);
     xmlhttp.onreadystatechange = function() {
       if(xmlhttp.readyState == 4) {
-	    if(xmlhttp.status == 200) {
-		  localStorage.setItem("userLogged", "");
-		  window.location.href = "/pages/index.html";
-		}
-		else{
-		  xmlhttp.responseText;
-		}
+        if(xmlhttp.status == 200) {
+          localStorage.setItem("userLogged", "");
+          window.location.href = "/pages/index.html";
+        }
+        else{
+          xmlhttp.responseText;
+        }
       }
     }
 
-	xmlhttp.open("POST", document.location.origin + "/rest/logout", true);
+    xmlhttp.open("POST", document.location.origin + "/rest/logout", true);
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-	xmlhttp.send(null);
-  }
+    xmlhttp.send(null);
+}
+
+  //FEEDS
+
+
+    //Events
+function postEvent(){
+
+    var title = document.getElementById("title").value;
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+    var location = document.getElementById("location").value;
+    var department = document.getElementById("department").value;
+    var isPublic = document.getElementById("isPublic").value;
+    var capacity = document.getElementById("capacity").value;
+    var isItPaid = document.getElementById("isItPaid").value;
+
+    var data = {
+    "title": title,
+    "startDate": startDate,
+    "endDate": endDate,
+    "location": location,
+    "department": department,
+    "isPublic": isPublic,
+    "capacity": capacity,
+    "isItPaid": isItPaid
+    };
+
+    var request = new XMLHttpRequest();
+
+    request.open("POST", document.location.origin + "/rest/feed/post/Event", true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify(data));
+    request.onreadystatechange  = function() {
+      if (request.readyState === 4 && request.status === 200) {
+          console.log(request.responseText);
+          console.log("SUCCESS");
+          alert(request.responseText);
+      } else if (request.readyState === 4) {
+          console.log(request.responseText);
+          console.log("FAIL");
+      }
+    };
+}
+
+function editEvent(){
+
+    var id = document.getElementById("eventID").value;
+    var title = document.getElementById("title").value;
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+    var location = document.getElementById("location").value;
+    var department = document.getElementById("department").value;
+    var isPublic = document.getElementById("isPublic").value;
+    var capacity = document.getElementById("capacity").value;
+    var isItPaid = document.getElementById("isItPaid").value;
+
+  var data = {
+  "title": title,
+  "startDate": startDate,
+  "endDate": endDate,
+  "location": location,
+  "department": department,
+  "isPublic": isPublic,
+  "capacity": capacity,
+  "isItPaid": isItPaid
+  };
+
+  var request = new XMLHttpRequest();
+
+  request.open("PATCH", document.location.origin + "/rest/feed/edit/Event" + id, true);
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  request.send(JSON.stringify(data));
+  request.onreadystatechange  = function() {
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request.responseText);
+        console.log("SUCCESS");
+        alert(request.responseText);
+    } else if (request.readyState === 4) {
+        console.log(request.responseText);
+        console.log("FAIL");
+    }
+  };
+}
+
+function deleteEvent(){
+
+      var id = document.getElementById("eventID").value;
+
+      var request = new XMLHttpRequest();
+
+      request.open("DELETE", document.location.origin + "/rest/feed/delete/Event" + id, true);
+      request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      request.send(JSON.stringify(null));
+      request.onreadystatechange  = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+            console.log("SUCCESS");
+            alert(request.responseText);
+        } else if (request.readyState === 4) {
+            console.log(request.responseText);
+            console.log("FAIL");
+        }
+      };
+    }
+
+    //FALTA QUERY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+
+  //News
+function postNews(){
+
+    var title = document.getElementById("title").value;
+
+    var data = {
+    "title": title
+    };
+
+    var request = new XMLHttpRequest();
+
+    request.open("POST", document.location.origin + "/rest/feed/post/News", true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify(data));
+    request.onreadystatechange  = function() {
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request.responseText);
+        console.log("SUCCESS");
+        alert(request.responseText);
+    } else if (request.readyState === 4) {
+        console.log(request.responseText);
+        console.log("FAIL");
+    }
+    };
+}
+
+function editNews(){
+
+    var id = document.getElementById("newsID").value;
+    var title = document.getElementById("title").value;
+
+    var data = {
+    "title": title
+    };
+
+    var request = new XMLHttpRequest();
+
+    request.open("PATCH", document.location.origin + "/rest/feed/edit/News" + id, true);
+    request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    request.send(JSON.stringify(data));
+    request.onreadystatechange  = function() {
+    if (request.readyState === 4 && request.status === 200) {
+        console.log(request.responseText);
+        console.log("SUCCESS");
+        alert(request.responseText);
+    } else if (request.readyState === 4) {
+        console.log(request.responseText);
+        console.log("FAIL");
+    }
+    };
+}
+
+function deleteNews(){
+
+      var id = document.getElementById("newsID").value;
+
+      var request = new XMLHttpRequest();
+
+      request.open("DELETE", document.location.origin + "/rest/feed/delete/News" + id, true);
+      request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      request.send(JSON.stringify(null));
+      request.onreadystatechange  = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+            console.log("SUCCESS");
+            alert(request.responseText);
+        } else if (request.readyState === 4) {
+            console.log(request.responseText);
+            console.log("FAIL");
+        }
+      };
+    }
+
+        //FALTA QUERY!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+        //USERS
+function modifyUserRole(){
+
+      var target = document.getElementById("target").value;
+      var newRole = document.getElementById("newRole").value;
+
+      var data = {
+            "target": target,
+            "newRole": newRole
+            };
+
+      var request = new XMLHttpRequest();
+
+      request.open("POST", document.location.origin + "/rest/modify/role", true);
+      request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      request.send(JSON.stringify(data));
+      request.onreadystatechange  = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+            console.log("SUCCESS");
+            alert(request.responseText);
+        } else if (request.readyState === 4) {
+            console.log(request.responseText);
+            console.log("FAIL");
+        }
+      };
+    }
+
+function queryUsers(){
+
+      var target = document.getElementById("target").value;
+      var newRole = document.getElementById("newRole").value;
+
+      var data = {
+            "target": target,
+            "newRole": newRole
+            };
+
+      var request = new XMLHttpRequest();
+
+      request.open("POST", document.location.origin + "/rest/modify/role", true);
+      request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      request.send(JSON.stringify(data));
+      request.onreadystatechange  = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+            console.log("SUCCESS");
+            alert(request.responseText);
+        } else if (request.readyState === 4) {
+            console.log(request.responseText);
+            console.log("FAIL");
+        }
+      };
+    }
+
+
+function deleteUser(){
+
+      var target = document.getElementById("target").value;
+
+      var data = {
+            "target": target
+            };
+
+      var request = new XMLHttpRequest();
+
+      request.open("DELETE", document.location.origin + "/rest/modify/delete", true);
+      request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      request.send(JSON.stringify(data));
+      request.onreadystatechange  = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.responseText);
+            console.log("SUCCESS");
+            alert(request.responseText);
+        } else if (request.readyState === 4) {
+            console.log(request.responseText);
+            console.log("FAIL");
+        }
+      };
+    }
