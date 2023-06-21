2a675dc27f869396bb02f52a9a9a5f9a707ffa0c
Manuel <m.jardim@campus.fct.unl.pt>
2023-06-21 12:16:27 +0100

    neew frontend

--- backend/src/main/java/filters/AdditionalResponseHeadersFilter.java
+++ backend/src/main/java/filters/AdditionalResponseHeadersFilter.java
@@ -16,8 +16,8 @@ public class AdditionalResponseHeadersFilter implements ContainerResponseFilter 
 			throws IOException {
 		responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
 		responseContext.getHeaders().add("Access-Control-Allow-Methods", "HEAD,GET,PUT,POST,DELETE,OPTIONS");
-		responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-		//responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://universe-fct.oa.r.appspot.com");
+		//responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+		responseContext.getHeaders().add("Access-Control-Allow-Origin", "https://universe-fct.oa.r.appspot.com");
 		responseContext.getHeaders().add("Access-Control-Allow-Headers", "Content-Type, X-Requested-With");   //Cookie
 	}
 }
