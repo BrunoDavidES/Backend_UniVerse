abbc202f478acba048990ef65211861ac4814d67
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-06-28 20:38:01 +0100

    EntityResource updated to FirebaseToken

--- backend/src/main/java/resources/EntityResource.java
+++ backend/src/main/java/resources/EntityResource.java
@@ -2,7 +2,7 @@ package resources;
 
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.cloud.datastore.*;
-import util.ValToken;
+import com.google.firebase.auth.FirebaseToken;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -13,34 +13,12 @@ import javax.ws.rs.core.Response;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import static util.AuthToken.*;
+import static util.Constants.*;
+
 @Path("/entity")
 @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
 public class EntityResource {
-
-
-    private static final String CAPI = "Your not one of us\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣞⣆⢀⣠⢶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-            "⠀⢀⣀⡤⠤⠖⠒⠋⠉⣉⠉⠹⢫⠾⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
-            "⢠⡏⢰⡴⠀⠀⠀⠉⠙⠟⠃⠀⠀⠀⠈⠙⠦⣄⡀⢀⣀⣠⡤⠤⠶⠒⠒⢿⠋⠈⠀⣒⡒⠲⠤⣄⡀⠀⠀⠀⠀⠀⠀\n" +
-            "⢸⠀⢸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠀⠴⠂⣀⠀⠀⣴⡄⠉⢷⡄⠚⠀⢤⣒⠦⠉⠳⣄⡀⠀⠀⠀\n" +
-            "⠸⡄⠼⠦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⡂⠠⣀⠐⠍⠂⠙⣆⠀⠀\n" +
-            "⠀⠙⠦⢄⣀⣀⣀⣀⡀⠀⢷⠀⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡇⠠⣀⠱⠘⣧⠀\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠈⠉⢷⣧⡄⢼⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⡈⠀⢄⢸⡄\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⡀⠃⠘⠂⠲⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠀⡈⢘⡇\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢫⡑⠣⠰⠀⢁⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⣸⠁\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣯⠂⡀⢨⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡆⣾⡄⠀⠀⠀⠀⣀⠐⠁⡴⠁⠀\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣧⡈⡀⢠⣧⣤⣀⣀⡀⢀⡀⠀⠀⢀⣼⣀⠉⡟⠀⢀⡀⠘⢓⣤⡞⠁⠀⠀\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢺⡁⢁⣸⡏⠀⠀⠀⠀⠁⠀⠉⠉⠁⠹⡟⢢⢱⠀⢸⣷⠶⠻⡇⠀⠀⠀⠀\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⡏⠈⡟⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⠁⠀⠻⣧⠀⠀⣹⠁⠀⠀⠀\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⡤⠚⠃⣰⣥⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⠼⢙⡷⡻⠀⡼⠁⠀⠀⠀⠀\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠟⠿⡿⠕⠊⠉⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⣾⠉⣹⣷⣟⣚⣁⡼⠁⠀⠀⠀⠀⠀\n" +
-            "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠙⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
-
-    private static final String TOKEN_NOT_FOUND = "Token not found";
-    private static final String BO = "BO";
-    private static final String ROLE = "role";
-    private static final String NICE_TRY = "Nice try but your not a capi person";
-
     private static final Logger LOG = Logger.getLogger(EntityResource.class.getName());
     private static final Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
@@ -49,23 +27,22 @@ public class EntityResource {
     @POST
     @Path("/new/{kind}/{key}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getReceivedInbox(@Context HttpServletRequest request, @PathParam("kind") String kind,@PathParam("key") String keyName, Map<String, String> attributes) {
+    public Response getReceivedInbox(@HeaderParam("Authorization") String token, @PathParam("kind") String kind,@PathParam("key") String keyName, Map<String, String> attributes) {
         LOG.fine("Attempt to create new entity");
 
-            Transaction txn = datastore.newTransaction();
-            try {
-                final ValToken validator = new ValToken();
-                DecodedJWT token = validator.checkToken(request);
+        FirebaseToken decodedToken = authenticateToken(token);
+        if(decodedToken == null) {
+            return Response.status(Response.Status.UNAUTHORIZED).entity("Invalid Token").build();
+        }
 
-                if (token == null) {
-                    LOG.warning(TOKEN_NOT_FOUND);
-                    return Response.status(Response.Status.FORBIDDEN).entity(TOKEN_NOT_FOUND).build();
-                }
+
+        Transaction txn = datastore.newTransaction();
+            try {
                 /*
                 Key userKey = datastore.newKeyFactory().setKind("User").newKey(token.getClaim("user").toString());
                 Entity user = txn.get(userKey);
                 */
-                if(!String.valueOf(token.getClaim(ROLE)).replaceAll("\"", "").equals(BO)){
+                if(!String.valueOf(getRole(decodedToken)).replaceAll("\"", "").equals(BO)){
                     txn.rollback();
                     LOG.warning(NICE_TRY);
                     return Response.status(Response.Status.BAD_REQUEST).entity(CAPI).build();
