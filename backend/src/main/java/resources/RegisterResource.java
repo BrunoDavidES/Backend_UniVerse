63038fb232ce51a7ba3d60c28176fe1dab6299e1
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-06-28 03:59:29 +0100

    Email verification v1.2.5

--- backend/src/main/java/resources/RegisterResource.java
+++ backend/src/main/java/resources/RegisterResource.java
@@ -1,6 +1,6 @@
 package resources;
 
-import com.google.appengine.api.mail.*;
+import com.google.api.client.util.Base64;
 import com.google.cloud.Timestamp;
 import com.google.cloud.datastore.*;
 import com.google.firebase.auth.FirebaseAuth;
@@ -10,12 +10,30 @@ import com.google.firebase.auth.UserRecord.CreateRequest;
 import com.google.firebase.database.FirebaseDatabase;
 import util.UserData;
 
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.logging.Logger;
+
+import com.google.api.services.gmail.model.Message;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 
 import static util.Constants.*;
 
@@ -32,7 +50,7 @@ public class RegisterResource {
     @POST
     @Path("/")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response register(UserData data) throws FirebaseAuthException {
+    public Response register(UserData data) throws FirebaseAuthException, MessagingException, IOException {
         LOG.fine("Attempt to register user: " + data.username);
 
         if (!data.validateRegister()) {
@@ -57,23 +75,68 @@ public class RegisterResource {
         return response;
     }
 
-    public void emailVerification(String email) throws FirebaseAuthException {
+    public void emailVerification(String email) throws FirebaseAuthException, MessagingException, IOException {
         String link = firebaseAuth.generateEmailVerificationLink(email);
 
+        String fromEmailAddress = "uni.capi.crew@gmail.com";
+
+        sendEmail(email, fromEmailAddress);
+
+    }
+
+    public static Message sendEmail(String fromEmailAddress, String toEmailAddress) throws MessagingException, IOException {
+        /* Load pre-authorized user credentials from the environment.
+           TODO(developer) - See https://developers.google.com/identity for
+            guides on implementing OAuth2 for your application.*/
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault()
+                .createScoped(GmailScopes.GMAIL_SEND);
+        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(credentials);
+
+        // Create the gmail API client
+        Gmail service = new Gmail.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Gmail samples")
+                .build();
+
+        // Create the email content
+        String messageSubject = "Test message";
+        String bodyText = "lorem ipsum.";
+
+        // Encode as MIME message
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(fromEmailAddress));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(toEmailAddress));
+        email.setSubject(messageSubject);
+        email.setText(bodyText);
+
+        // Encode and wrap the MIME message into a gmail message
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+
         try {
-            MailService.Message message = new MailService.Message();
-            message.setSender("from@example.com");
-            message.setTo(email);
-            message.setSubject("Example email");
-            message.setTextBody("Click the following link to verify your email:\n\n" + link);
-
-            MailService mailService = MailServiceFactory.getMailService();
-            mailService.send(message);
-
-            LOG.info("Verification email sent to: " + email);
-        } catch (Exception e) {
-            LOG.warning("Failed to send verification email to: " + email);
+            // Create send message
+            message = service.users().messages().send("me", message).execute();
+            LOG.info("Message id: " + message.getId());
+            LOG.info(message.toPrettyString());
+            return message;
+        } catch (GoogleJsonResponseException e) {
+            // TODO(developer) - handle error appropriately
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 403) {
+                LOG.warning("Unable to send message: " + e.getDetails());
+            } else {
+                throw e;
+            }
         }
+        return null;
     }
 
     private Response firebaseRegister(String username, String email, String password, String name, String role) {
