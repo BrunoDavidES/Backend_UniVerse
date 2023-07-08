6ef3fe3076835025153b25c3781871c6d998e046
Gonçalo Silva <gab.silva@campus.fct.unl.pt>
2023-07-08 19:32:31 +0100

    Password is hashed in frontend

--- backend/src/main/webapp/js/login.js
+++ backend/src/main/webapp/js/login.js
@@ -16,85 +16,96 @@ document.addEventListener("DOMContentLoaded", function() {
   loginForm.addEventListener("submit", function(event) {
     event.preventDefault();
     var username = document.getElementById("username").value;
-    var pass = document.getElementById("password").value;
+    var password = document.getElementById("password").value;
 
-    firebase.auth().signInWithEmailAndPassword(username, pass)
-      .then(function() {
-        document.getElementById("password").value = null;
-        sessionStorage.setItem("userLogged", username.split("@")[0]);
+    // Hash the password using SHA-256
+//    var encoder = new TextEncoder();
+//    var data = encoder.encode(password);
+//    crypto.subtle.digest("SHA-256", data)
+//     .then(function(digest) {
+        var hashedPassword = CryptoJS.SHA256(password).toString();
 
-        firebase.auth().currentUser.getIdToken()
-          .then(function(token) {
-            // Decode the token to access the payload
-            var decodedToken = decodeToken(token);
-            if (decodedToken && decodedToken.role !== "A" && decodedToken.role !== "BO") {
-              window.location.href = "/backoffice/index.html";
-              alert("Invalid role");
-              //sessionStorage.removeItem("capiToken");
-              //sessionStorage.removeItem("userLogged");
-              sessionStorage.clear();
-              localStorage.clear();
-            } else {
-              window.location.href = "/backoffice/mainPage.html";
-            }
+        // Continue with the authentication using the hashed password
+        firebase.auth().signInWithEmailAndPassword(username, hashedPassword)
+          .then(function() {
+            document.getElementById("password").value = null;
+            sessionStorage.setItem("userLogged", username.split("@")[0]);
+
+            firebase.auth().currentUser.getIdToken()
+              .then(function(token) {
+                // Decode the token to access the payload
+                var decodedToken = decodeToken(token);
+                if (decodedToken && decodedToken.role !== "A" && decodedToken.role !== "BO") {
+                  window.location.href = "/backoffice/index.html";
+                  alert("Invalid role");
+                  sessionStorage.clear();
+                  localStorage.clear();
+                } else {
+                  window.location.href = "/backoffice/mainPage.html";
+                }
+              })
+    //          .catch(function(error) {
+     //           sessionStorage.clear();
+       //         localStorage.clear();
+         //       console.error(error);
+            //  });
           })
           .catch(function(error) {
-            //sessionStorage.removeItem("capiToken");
-            //sessionStorage.removeItem("userLogged");
-            sessionStorage.clear();
-            localStorage.clear();
-            console.error(error);
+            console.error("Failed to sign in:", error);
           });
-
-      });
-
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        user.getIdToken().then(function(token) {
-          var existingToken = sessionStorage.getItem("capiToken");
-          if (existingToken) {
-            sessionStorage.removeItem("capiToken");
-          }
-          sessionStorage.setItem("capiToken", token);
-        }).catch(function(error) {
-          console.error(error);
-        });
-      }
-    });
-  });
-});
-
-  const togglePassword = document.querySelector('#togglePassword');
-  const password = document.querySelector('#password');
-
-  togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    // toggle the eye slash icon
-    this.classList.toggle('fa-eye-slash');
+      })
+    //  .catch(function(error) {
+  //      console.error("Failed to hash the password:", error);
+//      });
   });
 
   // Decode the token
   function decodeToken(token) {
-    // Implement your decoding logic here
-    // You can use a JWT library or a custom decoding function
-    // Example:
-    // var decodedToken = jwt_decode(token);
-    // return decodedToken;
+      // Implement your decoding logic here
+      // You can use a JWT library or a custom decoding function
+      // Example:
+      // var decodedToken = jwt_decode(token);
+      // return decodedToken;
 
-    // For simplicity, we'll assume the token contains the role information
-    // in the 'role' claim
-    try {
-      var payload = token.split('.')[1];
-      var decodedPayload = JSON.parse(atob(payload));
-      return decodedPayload;
-    } catch (error) {
-      console.error("Failed to decode token:", error);
-      return null;
-    }
+      // For simplicity, we'll assume the token contains the role information
+      // in the 'role' claim
+      try {
+        var payload = token.split('.')[1];
+        var decodedPayload = JSON.parse(atob(payload));
+        return decodedPayload;
+      } catch (error) {
+        console.error("Failed to decode token:", error);
+        return null;
+      }
 
-}
+  }
+
+  // Function to convert the digest to a hex-encoded string
+  function hexEncode(digest) {
+    var hex = Array.from(new Uint8Array(digest))
+      .map(function(byte) {
+        return byte.toString(16).padStart(2, "0");
+      })
+      .join("");
+    return hex;
+  }
+
+  // ...
+//});
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
+});
+
+  // Decode the token
+
 
 /*
 document.addEventListener("DOMContentLoaded", function() {
