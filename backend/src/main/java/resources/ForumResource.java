b3846f9474faea6e225dd3b55acd6cae96662377
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-07-11 23:12:59 +0100

    postID Date reverted

--- backend/src/main/java/resources/ForumResource.java
+++ backend/src/main/java/resources/ForumResource.java
@@ -486,7 +486,7 @@ public class ForumResource {
             String date = getCurrentDate();
 
             DatabaseReference forumRef = firebaseDatabase.getReference(FORUMS).child(forumID).child("feed");
-            String postID = date.replace("/", "-") + forumRef.push().getKey();
+            String postID = forumRef.push().getKey();
             forumRef.child(postID).child("author").child("username").setValueAsync(decodedToken.getUid());
             forumRef.child(postID).child("author").child("name").setValueAsync(decodedToken.getName());
             forumRef.child(postID).child("message").setValueAsync(data.getMessage());
